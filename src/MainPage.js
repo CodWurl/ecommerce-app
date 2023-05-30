29d9b008d6a0fc206e924ec519f1c8706bb70811
Michael Dixon <mdixon@kmha.com>
2023-05-30 12:08:36 -0400

    fixed ecommerce issue with switch component

---
+++ src/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the E-commerce Store!</h1>
+      <Link to="/payment">
+        <button>Pay Now</button>
+      </Link>
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ src/PaymentPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const PaymentPage = () => {
+  const handleToken = (token) => {
+    // Send the token to your server for processing
+    console.log(token);
+  };
+
+  return (
+    <div>
+      <h1>Enter Credit Card Information</h1>
+      <StripeCheckout
+        token={handleToken}
+        stripeKey="pk_test_51NAxatLUnhcBn6aERNPvJ6XIPOGfC9iNpZyQ9KIpaSsFpopIrSae1tA73kxbrc7Z0ln2WDW2DDp5e0yK4MGrxBXB00d7DtoT7h"
+      />
+    </div>
+  );
+};
+
+export default PaymentPage;
